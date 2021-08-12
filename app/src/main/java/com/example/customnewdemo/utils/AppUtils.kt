f38536948440d882876c45c0bfa9175e76b3a585
sssuuunnny <sunnygogo78@gmail.com>
2021-08-12 15:50:53 +0800

    sp dp 和px 之间的转换

---
+++ app/src/main/java/com/example/customnewdemo/utils/AppUtils.kt
@@ -0,0 +1,12 @@
+package com.example.customnewdemo.utils
+
+import android.content.Context
+import com.example.customnewdemo.app.MyApplication
+
+object AppUtils {
+
+    val appContext: Context
+        get() = MyApplication.appContext
+
+
+}
---
+++ app/src/main/java/com/example/customnewdemo/utils/UiUtil.kt
@@ -0,0 +1,51 @@
+package com.example.customnewdemo.utils
+
+import android.content.Context
+import android.widget.TextView
+
+
+fun Int.sp2px(): Int {
+    val scale = AppUtils.appContext.resources.displayMetrics.scaledDensity
+    return (this * scale + 0.5f).toInt()
+}
+
+
+fun Int.px2sp(): Int {
+    val scale = AppUtils.appContext.resources.displayMetrics.scaledDensity
+    return (this / scale + 0.5f).toInt()
+}
+
+
+fun Int.dip2px(): Int {
+    val scale = AppUtils.appContext.resources.displayMetrics.density
+    return (this * scale + 0.5f).toInt()
+}
+
+fun Int.px2dip(): Int {
+    val scale = AppUtils.appContext.resources.displayMetrics.density
+    return (this / scale + 0.5f).toInt()
+}
+
+
+fun Float.sp2px(): Int {
+    val scale = AppUtils.appContext.resources.displayMetrics.scaledDensity
+    return (this * scale + 0.5f).toInt()
+}
+
+
+fun Float.px2sp(): Int {
+    val scale = AppUtils.appContext.resources.displayMetrics.scaledDensity
+    return (this / scale + 0.5f).toInt()
+}
+
+
+fun Float.dip2px(): Int {
+    val scale = AppUtils.appContext.resources.displayMetrics.density
+    return (this * scale + 0.5f).toInt()
+}
+
+fun Float.px2dip(): Int {
+    val scale = AppUtils.appContext.resources.displayMetrics.density
+    return (this / scale + 0.5f).toInt()
+}
+
