86827020b1546ee082e2743e6f0220bde4584ae7
stormzhang <zq329051@outlook.com>
2021-08-18 00:36:43 +0800

    camera 相机保存相片 预览相片的尺寸

--- app/src/main/java/com/example/customnewdemo/camera/Camera2Helper2.kt
+++ app/src/main/java/com/example/customnewdemo/camera/Camera2Helper2.kt
@@ -1,16 +1,25 @@
 package com.example.customnewdemo.camera
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.graphics.ImageFormat
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
+import android.media.ImageReader
 import android.os.Handler
 import android.os.HandlerThread
 import android.util.Size
+import android.view.Surface
 import android.view.TextureView
+import androidx.core.content.ContextCompat
 import com.example.customnewdemo.utils.LogUtils
+import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * 相关
@@ -34,12 +43,13 @@ class Camera2Helper2(val context: Activity, private val mTextureView: TextureVie
     private var mCameraFaceing = CameraCharacteristics.LENS_FACING_BACK // 默认使用后置摄像头
 
     private var mCameraId = "0"
-    private lateinit var mCameraCharacteristics: CameraCharacteristics
+    private lateinit var mCameraCharacteristics: CameraCharacteristics // 用于描述摄像头的各种特性,
 
     private lateinit var mCameraManager: CameraManager
     private var mCameraSensorOrientation = 0  // 摄像头方向  初始为 0
     private var mDisplayRotation = context.windowManager.defaultDisplay.rotation // 获取手机的方向
 
+    private var mCameraDevice: CameraDevice? = null
 
     init {
         handleThread.start()
@@ -68,8 +78,9 @@ class Camera2Helper2(val context: Activity, private val mTextureView: TextureVie
     }
 
 
-    private val mPreviewSize: Size = Size(PREVIEW_WIDTH, PREVIEW_HEIGHT)
-    private val mSaveSize: Size = Size(SAVE_WIDTH, SAVE_HEIGHT)
+    private var mPreviewSize: Size = Size(PREVIEW_WIDTH, PREVIEW_HEIGHT)
+    private var mSaveSize: Size = Size(SAVE_WIDTH, SAVE_HEIGHT)
+    private var mImageReader: ImageReader? = null
 
     /**
      * 初始化 camera
@@ -113,8 +124,157 @@ class Camera2Helper2(val context: Activity, private val mTextureView: TextureVie
         val previewSize = configurationMap?.getOutputSizes(ImageFormat.JPEG)
         val saveSize = configurationMap?.getOutputSizes(SurfaceTexture::class.java)
 
+        val exchange =
+            exchangeWidthAndHeight(mDisplayRotation, mCameraSensorOrientation)
+
+        // 获取保存的尺寸
+        mSaveSize = getBestSize(
+            if (exchange) mSaveSize.height else mSaveSize.width,
+            if (exchange) mSaveSize.width else mSaveSize.height,
+            if (exchange) mSaveSize.height else mSaveSize.width,
+            if (exchange) mSaveSize.width else mSaveSize.height,
+            saveSize?.toList() ?: emptyList()
+        )
+
+        // 获取 预览尺寸
+        mPreviewSize = getBestSize(
+            if (exchange) mPreviewSize.height else mPreviewSize.width,
+            if (exchange) mPreviewSize.width else mPreviewSize.height,
+            if (exchange) mPreviewSize.height else mPreviewSize.width,
+            if (exchange) mPreviewSize.width else mPreviewSize.height,
+            previewSize?.toList() ?: emptyList()
+        )
+
+
+        mTextureView.surfaceTexture?.setDefaultBufferSize(mPreviewSize.width, mPreviewSize.height)
+        LogUtils.d(
+            TAG,
+            "预览最优尺寸 ：${mPreviewSize.width} * ${mPreviewSize.height}, 比例  ${mPreviewSize.width.toFloat() / mPreviewSize.height}"
+        )
+        LogUtils.d(
+            TAG,
+            "保存图片最优尺寸 ：${mSaveSize.width} * ${mSaveSize.height}, 比例  ${mSaveSize.width.toFloat() / mSaveSize.height}"
+        )
+
+        // 设置 reader
+
+        mImageReader =
+            ImageReader.newInstance(mSaveSize.width, mSaveSize.height, ImageFormat.JPEG, 1)
+
+        mImageReader?.setOnImageAvailableListener(onImageAvailableListener, mCameraHandler)
+
+        openCamera()
+    }
+
+
+    private val onImageAvailableListener = ImageReader.OnImageAvailableListener {
+        // 处理得到的照片
+    }
+
+
+    /**
+     * 开启相机
+     */
+    private fun openCamera() {
+        // 权限检查
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+
+        ) {
+            LogUtils.d(TAG, "没有相机权限")
+// 权限交给外层去申请 不在这里申请 否则 有其他跳转逻辑
+            return
+
+        }
+
+        mCameraManager.openCamera(mCameraId, object : CameraDevice.StateCallback() {
+            override fun onOpened(cameraDevice: CameraDevice) {
+
+                mCameraDevice= cameraDevice
+                // 创建预览会话
+
+            }
+
+            override fun onDisconnected(p0: CameraDevice) {
+
+            }
+
+            override fun onError(p0: CameraDevice, p1: Int) {
+
+            }
+        }, mCameraHandler)
 
     }
 
+    /**
+     * 根据提供的指向[displayRotation] 和 [sensorOrientation] 返回是否需要交换宽高
+     */
+    public fun exchangeWidthAndHeight(displayRotation: Int, sensorOrientation: Int): Boolean {
+
+        var exchange = false
+        when (displayRotation) {
+            Surface.ROTATION_0, Surface.ROTATION_270 -> {
+                if (sensorOrientation == 90 || sensorOrientation == 270) { // 横屏
+                    exchange = true
+                }
+            }
+            Surface.ROTATION_90, Surface.ROTATION_180 -> {
+                if (sensorOrientation == 0 || sensorOrientation == 180) {// 竖直方向
+                    exchange = true
+                }
+            }
+            else -> LogUtils.d("displayRotation  is  $displayRotation")
+        }
+
+        LogUtils.d(TAG, "屏幕方向 -> $displayRotation")
+        LogUtils.d(TAG, "相机方向 -> $sensorOrientation")
+        return exchange
+    }
+
+
+    /**
+     * 根据提供的参数值返回与指定宽高相等或最接近的尺寸
+     */
+    private fun getBestSize(
+        targetWidth: Int, targetHeight: Int,
+        maxWidth: Int, maxHeight: Int, sizeList: List<Size>
+    ): Size {
+
+        val bigEnough = ArrayList<Size>() // 比指定宽高大的size 列表
+
+        val notBigEnough = arrayListOf<Size>() // 比指定宽高小的size 列表
+
+        for (size in sizeList) {
+            if (size.width <= maxWidth && size.height <= maxHeight
+                && size.width == size.height * targetWidth / targetHeight
+            ) {
+                if (size.width >= targetWidth && size.height >= targetHeight) {
+                    bigEnough.add(size)
+                } else {
+                    notBigEnough.add(size)
+                }
+            }
+        }
+
+        return when {
+            bigEnough.size > 0 -> Collections.min(bigEnough, CompareSizesByArea())
+            notBigEnough.size > 0 -> Collections.max(notBigEnough, CompareSizesByArea())
+            else -> sizeList[0]
+
+        }
+
+    }
+
+
+    /**
+     * 比较规则
+     */
+    private class CompareSizesByArea : Comparator<Size> {
+        override fun compare(size1: Size, size2: Size): Int {
+            return java.lang.Long.signum(size1.width.toLong() * size1.height - size2.width.toLong() * size2.height)
+        }
+    }
 
 }
