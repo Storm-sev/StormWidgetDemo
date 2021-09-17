66e4579683ea52f8d29eb0bd6179fcdc40b53e0f
sssuuunnny <sunnygogo78@gmail.com>
2021-09-17 17:01:43 +0800

    test: 协程

--- app/src/main/java/com/example/customnewdemo/act/CoroutinesActivity.kt
+++ app/src/main/java/com/example/customnewdemo/act/CoroutinesActivity.kt
@@ -4,15 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.customnewdemo.R
 import com.example.customnewdemo.databinding.ActivityCoroutinesBinding
 import com.example.customnewdemo.utils.LogUtils
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.coroutineContext
+import kotlinx.coroutines.*
+
 
 /**
  * 协程相关
@@ -36,11 +31,27 @@ class CoroutinesActivity : AppCompatActivity() {
 
     private fun setUpData() {
 //        val context =  CoroutineContext()
-        val coroutineScope = CoroutineScope(Dispatchers.IO)
+//
+//        coroutineScope.launch {
+//            LogUtils.d(TAG, "运行的线程  ---")
+//
+//        }
 
-        coroutineScope.launch {
-            LogUtils.d(TAG, "运行的线程  ---")
+
+        val coroutineScope = MainScope()
+        coroutineScope.launch{
+           val str =  getName()
+            LogUtils.d(TAG, "获取到的信息 --> $str")
+
+
         }
+
+//        CoroutineScope.launch() { }
+    }
+
+    private suspend fun getName() = withContext(Dispatchers.IO){
+        LogUtils.d(TAG,"在子线程中执行 ")
+
 
     }
 
