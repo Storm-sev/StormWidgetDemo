18fef56e2ca44949c182707b9d6c7d90aa402c61
sssuuunnny <sunnygogo78@gmail.com>
2021-10-12 15:03:48 +0800

    feat. Glide 加载图片第一帧 style. 优化mainlayout的布局结构

--- app/src/main/java/com/example/customnewdemo/act/PermissionInfoActivity.kt
+++ app/src/main/java/com/example/customnewdemo/act/PermissionInfoActivity.kt
@@ -3,6 +3,9 @@ package com.example.customnewdemo.act
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.example.customnewdemo.R
 import com.example.customnewdemo.databinding.ActivityPermissionInfoBinding
 import com.example.customnewdemo.utils.AppUtils
@@ -32,6 +35,25 @@ class PermissionInfoActivity : AppCompatActivity() {
 
         }
 
+        binding.tvLoadVideo.setOnClickListener {
+
+            loadVideoPath()
+        }
+    }
+
+    private fun loadVideoPath() {
+
+        val  url = "http://ejz.yunchewen.com/ftp/auditorium/video/20211009/5626af2f021547399df874f8626bec6f.mp4"
+
+        val options = RequestOptions()
+        options.skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .centerCrop()
+
+        Glide.with(this)
+            .setDefaultRequestOptions(options)
+            .load(url)
+            .into(binding.ivVideoFirst)
 
     }
 
